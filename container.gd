33c04dee9eb748356ca535ad6c35ae0eb6ac3398
simi <smain8413@student.pps.net>
2025-10-24 15:34:05 -0700

    whatever

--- container.gd
+++ container.gd
@@ -2,22 +2,34 @@ extends Control
 # place objects
 
 @export var margin_y : float
+@export var button_padding : float
 const BUTTON_HEIGHT : int = 48
 var SCREEN_HEIGHT : float
-var CENTER_LINE : float
 var abs_max_height : float
 
+var children : Array[Button]
+
+func createChildren() -> void :
+	var buttons : Array[String] = ["Main Menu", "Settings"]
+	for i in range(len(buttons)):
+		var child : Button = Button.new()
+		child.text = buttons[i]
+		child.add_theme_font_size_override("pause_button", 48)
+		child.size += Vector2(20, 10)
+		children.append(child)
+
+		
 
 func _ready() -> void:
+	createChildren()
+	margin_y += BUTTON_HEIGHT/2.0
 	SCREEN_HEIGHT = get_viewport_rect().size.y
-	CENTER_LINE  = get_viewport_rect().get_center().y
-	print("Center line: " + str(CENTER_LINE))
 	abs_max_height = SCREEN_HEIGHT - margin_y
-	var children : Array[Node] = self.get_children()
-	var padding = (abs_max_height / len(children)) - (BUTTON_HEIGHT *  len(children))
+
 	for i in range(len(children)):
-		var child = children[i] 
+		var child : Button = children[i]
 		child.position.x = get_viewport_rect().get_center().x
-		child.position.y = (CENTER_LINE + .5 * BUTTON_HEIGHT) - (padding * i)
+		child.position.y = (( abs_max_height / len(children) * (i+0) ) +  margin_y)
+		child.position -= child.size / 2
 		print(child.position)
 	
