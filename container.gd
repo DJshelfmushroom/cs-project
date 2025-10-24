843f68b4559e3d64ff20c623170e4dc30ebb5f09
simi <smain8413@student.pps.net>
2025-10-24 14:16:23 -0700

    pause menu chabges

--- container.gd
+++ container.gd
@@ -2,7 +2,8 @@ extends Control
 # place objects
 
 @export var margin_y : float
-var SCREEN_HEIGHT : int
+const BUTTON_HEIGHT : int = 48
+var SCREEN_HEIGHT : float
 var CENTER_LINE : float
 var abs_max_height : float
 
@@ -10,16 +11,13 @@ var abs_max_height : float
 func _ready() -> void:
 	SCREEN_HEIGHT = get_viewport_rect().size.y
 	CENTER_LINE  = get_viewport_rect().get_center().y
-
-	
 	print("Center line: " + str(CENTER_LINE))
 	abs_max_height = SCREEN_HEIGHT - margin_y
-	var _children : Array[Node] = self.get_children()
+	var children : Array[Node] = self.get_children()
+	var padding = (abs_max_height / len(children)) - (BUTTON_HEIGHT *  len(children))
+	for i in range(len(children)):
+		var child = children[i] 
+		child.position.x = get_viewport_rect().get_center().x
+		child.position.y = (CENTER_LINE + .5 * BUTTON_HEIGHT) - (padding * i)
+		print(child.position)
 	
-	pass
-	
-func organizeChildren(children : Array[Node]):
-	for child in children:
-		
-		pass
-	pass
