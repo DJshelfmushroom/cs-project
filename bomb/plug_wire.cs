17291536aeb98a2c1083e51410bcf0b2c3d7dd9b
DJshelfmushroom <calvin@kmikeym.com>
2025-11-03 15:13:46 -0800

    organization

--- bomb/plug_wire.cs
+++ bomb/plug_wire.cs
@@ -1,19 +1,20 @@
 using Godot;
 using System;
 
-public partial class plug_wire : Path3D
+public partial class plug_wire : Node3D
 {
-	public RayCast3D ray;
+	public RayCast3D Ray;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		ray = GetNode<RayCast3D>("RayCast3D");
-		ray.Enabled = true;
-		GD.Print("test");
+		Ray = GetNode<RayCast3D>("RayCast3D");
+		Ray.Enabled = true;
+		
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+	
 }
