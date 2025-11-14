0303e22270c68ea631a5a57ecceb899fb5808e6c
DJshelfmushroom <calvin@kmikeym.com>
2025-11-14 14:44:36 -0800

    export templates

--- bomb/bomb.cs
+++ bomb/bomb.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+
 public partial class bomb : Node3D
 {
 	PackedScene plug_scene = GD.Load<PackedScene>("res://bomb/plug.tscn");
@@ -45,5 +46,68 @@ public partial class bomb : Node3D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+	}
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventMouseMotion motion)
+		{
+			if (Input.IsActionPressed("ui_mouse_right_button"))
+			{
+				var transform = this.Transform;
+				transform.Basis = transform.Basis.Rotated(Vector3.Up, motion.Relative.X * 0.01f);
+				transform.Basis = transform.Basis.Rotated(Vector3.Forward, motion.Relative.Y * 0.01f);
+				this.Transform = transform;
+				Vector2 mousePos = GetViewport().GetMousePosition();
+				float delta = (float)GetProcessDeltaTime();
+				Vector2 mouseVel = motion.Velocity;
+				Vector2 size = GetViewport().GetVisibleRect().Size;
+				int wrapTolerance = 32;
+				int velTolerance = 4;
+				GD.PrintRich($"Mouse Pos: [color=yellow]{mousePos}[/color]\n" +
+				             $"Viewport Size: [color=yellow]{size}[/color]\n" +
+				             $"Mouse Vel: [color=yellow]{mouseVel}[/color]\n" +
+				             $"Mouse Vel Adjusted: [color=yellow]{mouseVel * new Vector2(delta, delta)}[/color]\n" +
+				             $"Delta: [color=yellow]{delta}[/color]");
+				mouseVel *= new Vector2(delta, delta);
+				/*if ((mousePos.X >= size.X - wrapTolerance && mouseVel.X > velTolerance) || mousePos.X >= size.X - 8)
+			{
+				Input.WarpMouse(new Vector2(wrapTolerance + 1, mousePos.Y));
+				GD.PrintRich($"Wrapping to [color=green]({wrapTolerance}, {mousePos.Y})[/color]");
+			}
+			else if ((mousePos.X <= wrapTolerance && mouseVel.X < -velTolerance) || mousePos.X <= 8)
+			{
+				Input.WarpMouse(new Vector2(size.X - wrapTolerance, mousePos.Y));
+				GD.PrintRich($"Wrapping to [color=green]({size.X - wrapTolerance}, {mousePos.Y})[/color]");
+			}
+			else if ((mousePos.Y >= size.Y - wrapTolerance && mouseVel.Y > velTolerance) || mousePos.Y >= size.Y - 8)
+			{
+				Input.WarpMouse(new Vector2(mousePos.X, wrapTolerance));
+				GD.PrintRich($"Wrapping to [color=green]({mousePos.X}, {wrapTolerance})[/color]");
+			}
+			else if ((mousePos.Y <= wrapTolerance && mouseVel.Y < -velTolerance) || mousePos.Y <= 8)
+			{
+				Input.WarpMouse(new Vector2(mousePos.X, size.Y - wrapTolerance));
+				GD.PrintRich($"Wrapping to [color=green]({mousePos.X}, {size.Y - wrapTolerance})[/color]");
+			}*/
+				if (mousePos.X + mouseVel.X > size.X)
+				{
+					Input.WarpMouse(new Vector2(1, mousePos.Y));
+					GD.PrintRich("[color=green]wrapping[/color]");
+				}
+				else if (mousePos.X + mouseVel.X < 0)
+				{
+					Input.WarpMouse(new Vector2(size.X - 1, mousePos.Y));
+				}
+				else if (mousePos.Y + mouseVel.Y > size.Y)
+				{
+					Input.WarpMouse(new Vector2(mousePos.X, 1));
+				}
+				else if (mousePos.Y + mouseVel.Y < 0)
+				{
+					Input.WarpMouse(new Vector2(mousePos.X, size.Y - 1));
+				}
+			}
+		}
 	}
 }
