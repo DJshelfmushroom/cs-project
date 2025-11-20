60e94c640ee0000caabbcbfff806a996f2adbf58
Kohen Newmister <knewmister5249@student.pps.net>
2025-11-20 10:21:32 -0800

    small refactor (holy shit caleb) -Calvin

--- scripts/puzzles/puzzle_1.gd
+++ scripts/puzzles/puzzle_1.gd
@@ -2,26 +2,28 @@ extends Control
 
 var completed
 var strikeable = true
+@onready var TextEdit1 = $EquationLabel/AnswerLabel/TextEdit1
+@onready var TextEdit2 = $EquationLabel/AnswerLabel/TextEdit2
 
 func _ready() -> void:
 	completed = false
 
 func _process(_delta: float) -> void:
-	if (($EquationLabel/AnswerLabel/TextEdit1.text == str($EquationLabel.get_rand1()) && $EquationLabel/AnswerLabel/TextEdit2.text == str($EquationLabel.get_rand2())) \
-	or ($EquationLabel/AnswerLabel/TextEdit1.text == str($EquationLabel.get_rand2()) && $EquationLabel/AnswerLabel/TextEdit2.text == str($EquationLabel.get_rand1()))):
+	if ((TextEdit1.text == str($EquationLabel.get_rand1()) && TextEdit2.text == str($EquationLabel.get_rand2())) \
+	or (TextEdit1.text == str($EquationLabel.get_rand2()) && TextEdit2.text == str($EquationLabel.get_rand1()))):
 		completed = true
-	elif (not($EquationLabel/AnswerLabel/TextEdit1.text == "") && not($EquationLabel/AnswerLabel/TextEdit2.text == "") && strikeable == true):
-		$EquationLabel/AnswerLabel/TextEdit1.editable = false
-		$EquationLabel/AnswerLabel/TextEdit1.add_theme_color_override("font_readonly_color","red")
-		$EquationLabel/AnswerLabel/TextEdit2.editable = false
-		$EquationLabel/AnswerLabel/TextEdit2.add_theme_color_override("font_readonly_color", "red")
+	elif (not(TextEdit1.text == "") && not(TextEdit2.text == "") && strikeable == true):
+		TextEdit1.editable = false
+		TextEdit1.add_theme_color_override("font_readonly_color","red")
+		TextEdit2.editable = false
+		TextEdit2.add_theme_color_override("font_readonly_color", "red")
 		$"..".strikes += 1
 		strikeable = false
 		await get_tree().create_timer(1.0).timeout
-		$EquationLabel/AnswerLabel/TextEdit1.text = ""
-		$EquationLabel/AnswerLabel/TextEdit2.text = ""
-		$EquationLabel/AnswerLabel/TextEdit1.editable = true
-		$EquationLabel/AnswerLabel/TextEdit1.add_theme_color_override("font_readonly_color","green")
-		$EquationLabel/AnswerLabel/TextEdit2.editable = true
-		$EquationLabel/AnswerLabel/TextEdit2.add_theme_color_override("font_readonly_color", "green")
+		TextEdit1.text = ""
+		TextEdit2.text = ""
+		TextEdit1.editable = true
+		TextEdit1.add_theme_color_override("font_readonly_color","green")
+		TextEdit2.editable = true
+		TextEdit2.add_theme_color_override("font_readonly_color", "green")
 		strikeable = true
