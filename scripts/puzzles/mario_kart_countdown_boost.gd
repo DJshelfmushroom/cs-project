7f91daaff4467ac40f27bc05dd0e90c1649a8bef
Kohen Newmister <knewmister5249@student.pps.net>
2025-12-01 15:31:59 -0800

    Moved some stuff and started new puzzle

---
+++ scripts/puzzles/mario_kart_countdown_boost.gd
@@ -0,0 +1,12 @@
+extends Control
+
+func ready():
+	$MarioKartWii.position = Vector2(255, 255)
+	$MarioKart8Deluxe.position = Vector2(820, 255)
+
+func _on_mario_kart_wii_pressed() -> void:
+	pass # Replace with function body.
+
+
+func _on_mario_kart_8_deluxe_pressed() -> void:
+	pass # Replace with function body.
