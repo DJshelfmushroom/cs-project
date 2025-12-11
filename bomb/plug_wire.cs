2fe1bdcb8277af0a2c65149b28c4a68faaeeec3c
DJshelfmushroom <calvin@kmikeym.com>
2025-12-11 15:34:06 -0800

    nothing

--- bomb/plug_wire.cs
+++ bomb/plug_wire.cs
@@ -4,11 +4,15 @@ using System;
 public partial class plug_wire : Node3D
 {
 	public RayCast3D Ray;
+
+	private NodePath path;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Ray = GetNode<RayCast3D>("RayCast3D");
-		Ray.Enabled = true;
+		Ray = GetNode("../RayCast3D") as RayCast3D;
+		GD.Print($"ray:{Ray}");
+		path = (NodePath)GetMeta("link");
+		GD.Print($"path:{path}");
 		
 	}
 
