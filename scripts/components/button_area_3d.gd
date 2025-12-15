034febda8bff8b17dee513e9c0e031d5e54fa6f6
DJshelfmushroom <calvin@kmikeym.com>
2025-12-15 15:33:33 -0800

    Added 3D button

--- scripts/components/button_area_3d.gd
+++ scripts/components/button_area_3d.gd
@@ -4,22 +4,29 @@ func _ready():
 	input_event.connect(_on_input_event)
 	mouse_entered.connect(_on_hover)
 	mouse_exited.connect(_on_unhover)
-	$"..".get_node("SubViewport/ColorRect").color = Color(0.4,0.4,0.4)
+	#$"..".get_node("SubViewport/ColorRect").color = Color(0.4,0.4,0.4)
 
 
 func _on_input_event(camera, event, position, normal, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
+	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
-			_on_button_pressed()
+			if event.pressed:
+				_on_button_pressed()
+			elif !event.pressed:
+				_on_button_released()
 			
 			
 func _on_hover():
-	$"..".get_node("SubViewport/ColorRect").color = Color(0.5,0.5,0.5)
+	#$"..".get_node("SubViewport/ColorRect").color = Color(0.5,0.5,0.5)
+	pass
 
 
 func _on_unhover():
-	$"..".get_node("SubViewport/ColorRect").color = Color(0.4,0.4,0.4)	
-
+	#$"..".get_node("SubViewport/ColorRect").color = Color(0.4,0.4,0.4)	
+	pass
 
 func _on_button_pressed():
-	pass
+	$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))#.rotated_local(Vector3.LEFT, PI)
+
+func _on_button_released():
+	$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))#.rotated_local(Vector3.LEFT, PI)
