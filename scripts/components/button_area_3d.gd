6fa9cff1a823483df8880ad2308a0a2a940e6a1b
Cascab <caharter6440@student.pps.net>
2025-12-17 15:33:41 -0800

    3d puzzle

--- scripts/components/button_area_3d.gd
+++ scripts/components/button_area_3d.gd
@@ -1,32 +1,26 @@
 extends Area3D
 
+var pressed = false
+
 func _ready():
 	input_event.connect(_on_input_event)
-	mouse_entered.connect(_on_hover)
-	mouse_exited.connect(_on_unhover)
-	$"..".mesh.material.albedo_color = Color(0.3,0.3,0.3)
 
+func _process(_delta: float) -> void:
+	if pressed and !Input.is_action_pressed("ui_mouse_left_button"):
+		_on_button_released()
+		pressed = false
 
 func _on_input_event(camera, event, position, normal, shape_idx):
 	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			if event.pressed:
-				_on_button_pressed()
-			elif !event.pressed:
-				_on_button_released()
-			
-			
-func _on_hover():
-	if (input_event.get_object() == $"."):
-		$"..".mesh.material.albedo_color = Color(0.35,0.35,0.35)
+		if event.button_index == MOUSE_BUTTON_LEFT and !pressed and Input.is_action_just_pressed("ui_mouse_left_button"):
+			_on_button_pressed()
+			pressed = true
 
-
-func _on_unhover():
-	if (input_event.get_object() == $"."):
-		$"..".mesh.material.albedo_color = Color(0.3,0.3,0.3)
 
 func _on_button_pressed():
 	$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))#.rotated_local(Vector3.LEFT, PI)
-
+	$"../../.."._on_button_pressed($"../..".num)
+	
 func _on_button_released():
 	$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))#.rotated_local(Vector3.LEFT, PI)
+	$"../../.."._on_button_released($"../..".num)
--- scripts/puzzles/components/equation_label.gd
+++ scripts/puzzles/components/equation_label.gd
@@ -1,19 +1,19 @@
-extends Label
+extends Label3D
 
 var gen = RandomNumberGenerator.new()
-var rand1 = gen.randi_range(1,9)
-var rand2 = gen.randi_range(1,9)
+var x = gen.randi_range(1,9)
+var y = gen.randi_range(1,9)
 	
 func _ready() -> void:
-	if (rand1 > rand2):
-		text = "x + y" + " = " + str(rand1 + rand2) + "\n" + "x * y" + " = " + str(rand1 * rand2) + "\n" + "x > y"
-	elif (rand1 < rand2):
-		text = "x + y" + " = " + str(rand1 + rand2) + "\n" + "x * y" + " = " + str(rand1 * rand2) + "\n" + "x < y"
+	if (x > y):
+		text = "x + y" + " = " + str(x + y) + "\n" + "x * y" + " = " + str(x * y) + "\n" + "x > y"
+	elif (x < y):
+		text = "x + y" + " = " + str(x + y) + "\n" + "x * y" + " = " + str(x * y) + "\n" + "x < y"
 	else:
-		text = "x + y" + " = " + str(rand1 + rand2) + "\n" + "x * y" + " = " + str(rand1 * rand2) + "\n" + "x = y"
+		text = "x + y" + " = " + str(x + y) + "\n" + "x * y" + " = " + str(x * y) + "\n" + "x = y"
 
-func get_rand1() -> int:
-	return rand1
+func get_x() -> int:
+	return x
 	
-func get_rand2() -> int:
-	return rand2
+func get_y() -> int:
+	return y
