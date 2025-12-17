422725a42cd4a5ad86c7004fe8f32f3dff6ee073
DJshelfmushroom <calvin@kmikeym.com>
2025-12-17 15:35:15 -0800

    Added keypad and DebugDraw3D

--- bomb/plug_wire.cs
+++ bomb/plug_wire.cs
@@ -1,25 +1,32 @@
 using Godot;
 using System;
+using System.Diagnostics;
+
 
 public partial class plug_wire : Node3D
 {
 	public RayCast3D Ray;
 
 	private NodePath path;
+
+	private Node3D pnode;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Ray = GetNode("../RayCast3D") as RayCast3D;
+		Ray = GetNode("RayCast3D") as RayCast3D;
 		GD.Print($"ray:{Ray}");
 		path = (NodePath)GetMeta("link");
 		GD.Print($"path:{path}");
-		
+		pnode = GetNode(path) as Node3D;
+		// Ray.TargetPosition = pnode.Position;
+
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		
+		//DebugDraw3D.DrawLine(Ray.GlobalPosition, pnode.GlobalPosition, Colors.Blue);
+		DebugDraw3D.DrawPoints([GetNode<Node3D>("../../bomb_instance/Node3D").GlobalPosition], DebugDraw3D.PointType.TypeSphere, 0.01f, Colors.Green);
 	}
 	
 }
--- scripts/components/button_area_3d.gd
+++ scripts/components/button_area_3d.gd
@@ -2,9 +2,9 @@ extends Area3D
 
 func _ready():
 	input_event.connect(_on_input_event)
-	mouse_entered.connect(_on_hover)
-	mouse_exited.connect(_on_unhover)
-	$"..".mesh.material.albedo_color = Color(0.3,0.3,0.3)
+	#mouse_entered.connect(_on_hover)
+	#mouse_exited.connect(_on_unhover)
+	#$"..".mesh.material.albedo_color = Color(0.3,0.3,0.3)
 
 
 func _on_input_event(camera, event, position, normal, shape_idx):
@@ -13,20 +13,18 @@ func _on_input_event(camera, event, position, normal, shape_idx):
 			if event.pressed:
 				_on_button_pressed()
 			elif !event.pressed:
-				_on_button_released()
+				_on_button_pressed()
 			
 			
-func _on_hover():
-	if (input_event.get_object() == $"."):
-		$"..".mesh.material.albedo_color = Color(0.35,0.35,0.35)
-
-
-func _on_unhover():
-	if (input_event.get_object() == $"."):
-		$"..".mesh.material.albedo_color = Color(0.3,0.3,0.3)
+#func _on_hover():
+	#if (input_event.get_object() == $"."):
+		#$"..".mesh.material.albedo_color = Color(0.35,0.35,0.35)
+#
+#
+#func _on_unhover():
+	#if (input_event.get_object() == $"."):
+		#$"..".mesh.material.albedo_color = Color(0.3,0.3,0.3)
 
 func _on_button_pressed():
-	$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))#.rotated_local(Vector3.LEFT, PI)
-
-func _on_button_released():
-	$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))#.rotated_local(Vector3.LEFT, PI)
+	pass
+	#$"..".transform = $"..".transform.scaled(Vector3(1,1,-1))
