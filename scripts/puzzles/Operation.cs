e20a621d1be40777e7edbf7c54fa71a9811e1b2b
simi <smain8413@student.pps.net>
2025-12-19 15:34:14 -0800

    BACK WHERE I WAS
    Why did you have to do that Calvin  D:

--- scripts/puzzles/Operation.cs
+++ scripts/puzzles/Operation.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Godot;
 using Godot.NativeInterop;
@@ -9,20 +10,26 @@ public partial class Operation : Node
 {
 	override public void _Ready()
 	{
-		new OperationPath2D(new Vector2(), new Vector2(), new Vector2(), 0, 1);
+		OperationPath2D operation = new OperationPath2D(new Vector2(200, 500), new Vector2(20, 20), new Vector2(), 5, 10, 30);
+		Path2D curve = operation.GeneratePath();
+		
+		Line2D line = new Line2D();
+		line.Points = curve.GetCurve().GetBakedPoints();
+		line.Width = 2;
+		
+		AddChild(line);
 	}
 }
 
 partial class OperationPath2D
 {
 	private Vector2 _start, _end, _size;
-	private int _spacing, _pointCount;
+	private int _spacing, _pointCount, _stepSize;
 
-	public Path2D GeneratePath(bool render = false) // render is for debugging
+	public Path2D GeneratePath() // render is for debugging
 	{
 		Path2D path = new Path2D();
 		path.Curve = GenerateCurve();
-
 		return path;
 	}
 
@@ -30,30 +37,38 @@ partial class OperationPath2D
 	{
 		Curve2D curve = new Curve2D();
 		Vector2[] points = new Vector2[_pointCount];
-		points.Append(_start);
+		points[0] = _start;
+		Vector2 previousPoint, newPoint = new Vector2();
+		float? angle;
 		for (int i = 1; i < _pointCount - 1; i++)
 		{
-			float t = (float)i / (_pointCount - 1);
-			// Vector2 point = Vector2.Lerp(_end, t); // i ont even care
-			//point.X += (float) GD.RandRange(-_size.X / 2, _size.X / 2) * (_spacing / 10f);
-			//point.Y += (float) GD.RandRange(-_size.Y / 2, _size.Y / 2) * (_spacing / 10f);
-			//points.Append(point);
+			previousPoint = points[i-1];
+			angle = Random.Shared.NextSingle() * MathF.PI * 2;
+			newPoint.X = previousPoint.X + MathF.Cos((float)angle) * _stepSize;
+			newPoint.Y = previousPoint.Y + MathF.Sin((float)angle) * _stepSize;
+			GD.Print($"Generated point {i}: {newPoint}");
+			points[i] = (newPoint);
 		}
-		
-		points.Append(_end);
+		points[_pointCount - 1] = _end;
+		angle = null;
+		foreach (Vector2 point in points) 
+		{
+			GD.Print($"Generated point {point}");
+			curve.AddPoint(point);
+			
+		}
 		return curve;
 	}
 
-	public OperationPath2D(Vector2 start, Vector2 end, Vector2 size, int spacing, int pointCount)
+	public OperationPath2D(Vector2 start, Vector2 end, Vector2 size, int spacing, int pointCount, int stepSize)
 	{
 		_start = start;
 		_end = end;
 		_size = size;
 		_spacing = spacing;
 		_pointCount = pointCount;
+		_stepSize = stepSize;
 	}
-
-	
 //	 DON'T ever let this out
 //	public unsafe OperationPath2D(Vector2 start, Vector2 end, Vector2 size, int spacing, int pointCount)
 //	{
