33ff18fc04cfe00811f7a8b2fe31dfa429866af1
simi <smain8413@student.pps.net>
2026-01-14 15:34:59 -0800

    work on rotateaxis
    simi (also bomb.tscn is not the file anymore so move the changes somehow)

--- bomb/Button/Models/textures/Roltateaxis.cs
+++ bomb/Button/Models/textures/Roltateaxis.cs
@@ -3,9 +3,18 @@ using System;
 
 public partial class Roltateaxis : Node3D
 {
-    public override void _Process(double delta)
-    {
-        base._Process(delta);
-        GetNode("../../Bomb_instance").Call("GetBombRotation");
-    }
+	public override void _Process(double delta)
+	{
+		base._Process(delta);
+		try
+		{
+			Vector3 bombRotation = (Vector3) GetNode("../../bomb_instance").Call("GetBombRotation");
+			GlobalRotation = bombRotation;
+		}
+		catch (Exception _) // what is type cast exception
+		{
+			return;
+		}
+	}
+	
 }
