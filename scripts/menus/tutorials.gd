2409f63df15c2440127d0fc2bdb6a630eac19493
Kohen Newmister <knewmister5249@student.pps.net>
2026-02-09 15:34:26 -0800

    Readded Numerle Tutorial+added Segment Tutorial+added green cursor image

--- scripts/menus/tutorials.gd
+++ scripts/menus/tutorials.gd
@@ -23,3 +23,6 @@ func _on_puzzle_4_tutorial_button_button_up() -> void:
 	
 func _on_puzzle_5_tutorial_button_button_up() -> void:
 	get_tree().change_scene_to_file("res://scenes/puzzles/tutorials/puzzle_5_tutorial.tscn")
+
+func _on_puzzle_6_tutorial_button_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/puzzles/tutorials/puzzle_6_tutorial.tscn")
