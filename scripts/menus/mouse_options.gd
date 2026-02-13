285e68af28a25a5b068fd2f4ef4c2a8531641a5b
Kohen Newmister <knewmister5249@student.pps.net>
2026-02-13 15:29:00 -0800

    Finished mouse cursor menu, added disable tutorial

--- scripts/menus/mouse_options.gd
+++ scripts/menus/mouse_options.gd
@@ -4,32 +4,60 @@ var red_arrow = load("res://assets/cursor/RedWire_Cursor.png")
 var red_hand = load("res://assets/cursor/RedWire_Hand.png")
 
 var green_arrow = load("res://assets/cursor/Green_RedWire_Cursor.png")
-var green_hand = load("res://assets/cursor/Green_RedWire_Cursor.png")
+var green_hand = load("res://assets/cursor/Green_RedWire_Hand.png")
+
+var black_arrow = load("res://assets/cursor/Black_RedWire_Cursor.png")
+var black_hand = load("res://assets/cursor/Black_RedWire_Hand.png")
 
 var mouse_cursor = null
 
 
-func _ready() -> void:
+func _ready():
 	place_set()
+	prepare_buttons()
+	
 	
 func check_mouse_cursor():
-	return $MainMenu/CustomCursor.cursor_color
+	return $CustomCursor.cursor_color
+
+func prepare_buttons():
+	if (SaveManager.level < 3):
+		set_green_to_base()
+	if (SaveManager.level < 8):
+		set_black_to_base()
 
 func place_set():
 	if (check_mouse_cursor() == "red"):
-		$Set.position = Vector2($Red_Mouse.position.x + 115, $Red_Mouse.position.y + 140)
-	if (check_mouse_cursor() == "green"):
-		$Set.position = Vector2($Green_Mouse.position.x + 115, $Green_Mouse.position.y + 140)
-		
+		$Set.position = Vector2($Red_Mouse.position.x + 85, $Red_Mouse.position.y + 140)
+		$Set.show()
+	elif (check_mouse_cursor() == "green"):
+		$Set.position = Vector2($Green_Mouse.position.x + 85, $Green_Mouse.position.y + 140)
+		$Set.show()
+	elif (check_mouse_cursor() == "black"):
+		$Set.position = Vector2($Black_Mouse.position.x + 85, $Black_Mouse.position.y + 140)
+		$Set.show()
+	else:
+		$Set.hide()
+	
 
-
+func set_green_to_base():
+	$Green_Mouse.text = "Unlocks at Level 3"
+	$Green_Mouse.disabled = true
+func set_black_to_base():
+	$Black_Mouse.text = "Unlocks at Level 8"
+	$Black_Mouse.disabled = true
 
 func _on_red_mouse_pressed() -> void:
-	Input.set_custom_mouse_cursor(red_arrow, Input.CURSOR_ARROW)
-	Input.set_custom_mouse_cursor(red_hand, Input.CURSOR_POINTING_HAND)
+	$CustomCursor.set_mouse_cursor(red_arrow, red_hand, "red")
 	place_set()
 	
 func _on_green_mouse_pressed() -> void:
-	Input.set_custom_mouse_cursor(green_arrow, Input.CURSOR_ARROW)
-	Input.set_custom_mouse_cursor(green_hand, Input.CURSOR_POINTING_HAND)
+	$CustomCursor.set_mouse_cursor(green_arrow, green_hand, "green")
 	place_set()
+
+func _on_black_mouse_pressed() -> void:
+	$CustomCursor.set_mouse_cursor(black_arrow, black_hand, "black")
+	place_set()
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/menus/main_menu.tscn")
