6a82dc3875a7ad1484ba66b6a1d94c1940207ebe
DJshelfmushroom <calvin@kmikeym.com>
2026-02-18 15:33:37 -0800

    working on zoom

--- bomb/CameraControl.cs
+++ bomb/CameraControl.cs
@@ -1,7 +1,12 @@
+using System;
+using System.Linq.Expressions;
 using Godot;
+using v3 = Godot.Vector3;
+using dd3d = DebugDraw3D;
 
 public partial class CameraControl : Camera3D
 {
+	private static readonly Action<String> log = Godot.GD.Print;
 	public void Zoom(float amount)
 	{
 		// 3 methods:
@@ -9,20 +14,47 @@ public partial class CameraControl : Camera3D
 		// this.Fov = Mathf.Clamp(this.Fov + amount, -500, 120);
 		// Move towards forward
 		//Transform = Transform.Translated(Vector3.ModelLeft * amount);
-		// Move toward bomb
+		// Move towards bomb
 		var bomb = GetNode<Node3D>("../bomb_instance");
-		Vector3 direction = (bomb.GlobalPosition - this.GlobalPosition).Normalized();
+		v3 direction = (bomb.GlobalPosition - this.GlobalPosition).Normalized();
 		float distToBomb = (bomb.GlobalPosition - this.GlobalPosition).Length();
+		var ray = new RayCast3D();
+		ray.Enabled = true;
+		AddChild(ray);
+		ray.GlobalPosition = this.GlobalPosition;
+		// ensure the ray will detect both bodies and areas and check all layers while debugging
+		ray.CollideWithBodies = true;
+		//ray.CollideWithAreas = true;
+		ray.CollisionMask = uint.MaxValue;
 
+		// TargetPosition is local to the RayCast node, so set it to the vector from the ray origin to the bomb
+		ray.TargetPosition = bomb.GlobalPosition - ray.GlobalPosition;
+		ray.ForceRaycastUpdate();
+		dd3d.DrawLine(ray.GlobalPosition, bomb.GlobalPosition, Colors.Green, 1f);
+		if (ray.IsColliding())
+		{
+			v3 point = ray.GetCollisionPoint();
+			float distToColPoint = (point - this.GlobalPosition).Length();
+			log("😭" + point);
+			DebugDraw3D.DrawPoints([point], DebugDraw3D.PointType.TypeSphere, 0.01f, Colors.Red, (float)base.GetProcessDeltaTime());
+			if (amount < 0 && distToColPoint <= 0.1f)
+			{
+				ray.QueueFree();
+				return;
+			}
+		}
 		// Don't let the camera zoom closer than this distance to the bomb
+		/*
 		if (amount < 0 && distToBomb <= 0.6f)
 		{
 			return;
 		}
-
+		*/
 		Transform = Transform.Translated(direction * -amount);
+		ray.QueueFree();
+
 	}
-	
+
 	public void Pan(float x, float y)
 	{
 		// pan the camera by moving it along its local axes
