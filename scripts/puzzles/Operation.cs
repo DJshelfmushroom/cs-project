aeb7c24ccdaedbfe79978352a264c696642f1e06
Simi <smain8413@student.pps.net>
2026-02-25 19:52:19 -0800

    Update Operation.cs
    it crashes the game now

--- scripts/puzzles/Operation.cs
+++ scripts/puzzles/Operation.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using csproject.scripts.core;
 using Godot;
 // ReSharper disable ReturnValueOfPureMethodIsNotUsed
@@ -8,18 +9,13 @@ namespace csproject.scripts.puzzles;
 
 public partial class Operation : Node
 {
-	private OperationPath2D operation;
+	private OperationPath2D _operation;
 	override public void _Ready()
 	{
-		operation = new OperationPath2D(new Vector2(500, 500), new Vector2(500, 500),
-			15,new Vector2(50, 200));
-		Path2D curve = operation.GeneratePath();
+		_operation = new OperationPath2D(new Vector2(500, 500), new Vector2(500, 500),
+			15,new Vector2(50, 200), this);
+		GenerateLine();
 		
-		Line2D line = new Line2D();
-		line.Points = curve.GetCurve().GetBakedPoints();
-		line.Width = 5;
-		
-		AddChild(line);
 	}
 
 	public override void _UnhandledInput(InputEvent @event)
@@ -28,19 +24,32 @@ public partial class Operation : Node
 		if (@event.IsActionPressed("key_x"))
 		{
 			Utils.Log("refresh",  this);
-			foreach (Node child in this.GetChildren())
-			{
-				this.RemoveChild(child);
-			}
-			Path2D curve = operation.GeneratePath();
-		
-			Line2D line = new Line2D();
-			line.Points = curve.GetCurve().GetBakedPoints();
-			line.Width = 5;
-		
-			AddChild(line);
-			
+			GenerateLine();
 		}
+	}
+
+	private void GenerateLine() 
+	{
+		foreach (Node child in this.GetChildren())
+		{
+			RemoveChild(child);
+		}
+		Line2D line = new Line2D();
+		// place:
+		try
+		{
+			line.Points = _operation.GenerateCurve().GetBakedPoints();
+		}
+		catch (StackOverflowException)
+		{
+			goto place;	
+		}
+
+		line.Width = 5;
+		
+		AddChild(line);
+		place:
+		Utils.Log("uh oh", this, "ERROR");
 	}
 }
 
@@ -49,11 +58,13 @@ class OperationPath2D(
 	Vector2 size,
 	int pointCount,
 	Vector2 pointSpaceRange,
+	Node owner,
 	OperationPath2D.StartPoint startPoint = OperationPath2D.StartPoint.BottomLeft)
 {
 	private Vector2 _center = center;
 	private Vector2 _size = size;
 	private StartPoint _startPoint = startPoint;
+	private const int ATTEMPT_THRESHOLD = 50;
 
 	public enum StartPoint
 	{
@@ -62,30 +73,105 @@ class OperationPath2D(
 		BottomLeft,
 		BottomRight
 	}
-
-	public Path2D GeneratePath()
+	
+	public Curve2D GenerateCurve()
 	{
-		var path = new Path2D();
-		path.SetCurve(GenerateCurve());
-		return path;
-	}
-
-	private Curve2D GenerateCurve()
-	{
+		var startChildren = owner.GetChildren();
 		Curve2D curve = new Curve2D();
 		var points = new List<Vector2> { new(0, 0) };
 		Random random = new Random();
 		int rBottom = (int) pointSpaceRange.X;
 		int rRange = Math.Abs((int)pointSpaceRange.Y - (int)pointSpaceRange.X);
-		for (int i = 1; i < pointCount; i++) //TODO add raycast check for interferences and re-roll if there are any
+		var prevDir = new Vector2(0, 0);
+		int attemptsAbsolute = 0;
+		for (int i = 1; i < pointCount; i++) //TODO make size work, figure out a way to optimize
 		{
 			int pointDist = (int)(rRange * random.NextSingle() + rBottom);
-			int d1 = random.Next(0, 3) - 1;
-			// Utils.Log($"d1 val: {d1}", "scripts/puzzles/Operation");
-			Vector2 dir = new Vector2(d1, d1 == 0 ? WierdRound(random.NextSingle() - .5f) : 0);
-			Utils.Log($"dir: {dir}", "scripts/puzzles/Operation");
-			var newPoint = dir *  pointDist;
-			points.Add(newPoint);
+			Vector2 dir;
+			int attempts = 0;
+			attemptsAbsolute++;
+
+			curve_roll:
+			do
+			{
+				attempts++;
+				do
+				{
+					int d1 = random.Next(0, 3) - 1;
+					dir = new Vector2(d1, d1 == 0 ? WierdRound(random.NextSingle() - .5f) : 0);
+				} while (dir.IsEqualApprox(-1 * prevDir) || dir.IsEqualApprox(prevDir));
+
+				prevDir = dir;
+				// Utils.Log($"dir: {dir}", "scripts/puzzles/Operation");
+				var newPoint = dir * pointDist + points[i - 1];
+				points.Add(newPoint);
+
+				var polyline = Geometry2D.OffsetPolyline([points[i - 1], newPoint], 5);
+				var collision = new CollisionPolygon2D();
+				collision.Polygon = polyline.ToArray().First();
+				var body = new StaticBody2D();
+				body.AddChild(collision);
+				owner.AddChild(body);
+
+				var ray = new RayCast2D();
+				ray.Position = points[i - 1];
+				ray.TargetPosition = newPoint;
+				ray.CollideWithBodies = true;
+				ray.CollideWithAreas = false;
+				ray.Enabled = true;
+				ray.ExcludeParent = true;
+				owner.AddChild(ray);
+				ray.ForceRaycastUpdate();
+				if (ray.IsColliding())
+				{
+					if (ray.GetCollider() is StaticBody2D && owner.GetChildren().Contains(ray.GetCollider()))
+					{
+						Utils.Log("Collision", owner);
+						owner.RemoveChild(ray);
+						continue;
+					}
+				}
+				owner.RemoveChild(ray);
+				break;
+			} while (attempts < ATTEMPT_THRESHOLD);
+
+			if (attempts == ATTEMPT_THRESHOLD)
+			{
+				attempts -= ATTEMPT_THRESHOLD / 10;
+				Utils.Log("attempts at limit", owner ,"WARN");
+				pointDist -= pointDist > 10? 10 : 0;
+				if (pointDist == 0)
+				{
+					points.RemoveAt(points.Count - 1);
+				}
+
+				goto curve_roll;
+			} 
+			if (attemptsAbsolute > 50)
+			{
+				Utils.Log("Attempts past threshold", owner, "ERROR");
+				i = 0;
+				foreach (var child in owner.GetChildren())
+				{
+					if (startChildren.Contains(child)) continue;
+					owner.RemoveChild(child);
+				}
+
+				attemptsAbsolute = -10;
+			}
+
+			if (attemptsAbsolute < 0)
+			{
+				throw new StackOverflowException();
+				return null;
+			}
+
+		}
+
+		foreach (var child in owner.GetChildren())
+		{
+			if (startChildren.Contains(child)) continue;
+			owner.RemoveChild(child);
 		}
 
 		foreach (var point in points)
@@ -95,7 +181,16 @@ class OperationPath2D(
 		return curve;
 	}
 
-	private int WierdRound(float i)
+	private void RaycastSection(Vector2 from,  Vector2 to)
+	{
+		var ray = new RayCast2D();
+		ray.Position = from;
+		ray.TargetPosition = to;
+		
+		
+	}
+
+	private static int WierdRound(float i)
 	{
 		return (int)(Math.Sign(i) * Math.Ceiling(MathF.Abs(i)));
 	}
