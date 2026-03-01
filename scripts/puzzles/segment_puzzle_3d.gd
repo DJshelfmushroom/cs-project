8ff104ce51b04b763becd213eeb1b666e29bf99e
caleb <caharter6440@student.pps.net>
2026-02-28 23:45:11 -0800

    couple lil tweaks

--- scripts/puzzles/segment_puzzle_3d.gd
+++ scripts/puzzles/segment_puzzle_3d.gd
@@ -23,7 +23,7 @@ var practice = true
 
 func _ready() -> void:
 	$Screen3D.set_size(2,1)
-	$Screen3D.position = Vector3(0.07, 0.17, 0.001)
+	$Screen3D.position = Vector3(0.07, 0.32, 0.001)
 	for x in range(2):
 		var gen = RandomNumberGenerator.new()
 		var rand = gen.randi_range(0, allnums.size() - 1)
